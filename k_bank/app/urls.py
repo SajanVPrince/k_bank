f3e5497a16bf3c4eca0be6de48b72b49bc4389dd
SajanVPrince <sajanvprince@gmail.com>
2025-02-11 20:47:42 +0530

    pages added

--- k_bank/app/urls.py
+++ k_bank/app/urls.py
@@ -4,5 +4,9 @@ from . import views
 urlpatterns = [
 
     path('',views.home),
+    path('money',views.money),
+    path('mini',views.mini),
+
+
     
 ]
--- k_bank/app/views.py
+++ k_bank/app/views.py
@@ -3,4 +3,10 @@ from django.shortcuts import render
 # Create your views here.
  
 def home(req):
-    return render(req,'index.html')
+    return render(req,'users/index.html')
+
+def money(req):
+    return render(req,'users/money.html')
+
+def mini(req):
+    return render (req,'users/mini.html')
